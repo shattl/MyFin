be0bfd3e752283039a7dc195a8b9081d89ee4273
anon <anon@krn>
2010-11-05 22:08:28 +0300

    ???????? ????????? ???????

--- app/view/renderTags.php
+++
@@ -1,3 +0,0 @@
-<?php foreach ($tag_list as $tag) : ?>
-    <a href="?by_tag=<?php echo $tag['id']?>"><?php echo $tag['name']?></a>
-<?php endforeach;
--- index.php
+++ index.php
@@ -7,7 +7,28 @@ require_once 'app/init.php';
 
 Page::set_title( 'Мои финансы' );
 
-$events_list = Db::selectGetArray( 'SELECT * FROM `events` ORDER BY date DESC' );
+/* Получение списка событий
+ */
+
+$where = array();
+
+if (isset($_GET['date_start']))
+    $where[] = Db::buildReq('events.date > FROM_UNIXTIME(@i)', strtotime($_GET['date_start']));
+
+if (isset($_GET['date_end']))
+    $where[] = Db::buildReq('events.date < FROM_UNIXTIME(@i)', strtotime($_GET['date_end']));
+
+if (isset($_GET['by_tag']))
+    $sql = 'SELECT events.* FROM `events`, `ev2tag` WHERE '
+        . Db::buildReq('ev2tag.ev_id = events.id AND ev2tag.tag_id = @i', $_GET['by_tag'])
+        . (count($where) > 0 ? ' AND ' . implode (' AND ', $where) : '')
+        . ' ORDER BY events.date DESC';
+else
+    $sql = 'SELECT * FROM `events` '
+        . (count($where) > 0 ? 'WHERE ' . implode (' AND ', $where) : '')
+        . ' ORDER BY date DESC';
+
+$events_list = Db::selectGetArray( $sql );
 
 $total_in = 0;
 $total_out = 0;
@@ -30,10 +51,83 @@ foreach ($events_list as $id => $event) {
             . ' tags.id = ev2tag.tag_id AND ev2tag.ev_id = @i', $event['id'] );
 }
 
-$total_in = $total_in / 100.0;
-$total_out = 0 - $total_out / 100.0;
-
 Page::addVar( 'events_list', $events_list );
-Page::addVar( 'total_in', $total_in );
-Page::addVar( 'total_out', $total_out );
+Page::addVar( 'total_in', $total_in / 100.0 );
+Page::addVar( 'total_out', 0 - $total_out / 100.0 );
+
+/* Построение ссылок для выборок по времени
+ */
+
+$date_links['today'] = '?date_start=' . date('Y:m:d H:i:s', mktime(0, 0, 0))
+        . '&date_end=' . date('Y:m:d H:i:s');
+$date_links['mouth'] = '?date_start=' . date('Y:m:d H:i:s', mktime(0, 0, 0, date("n"), 1))
+        . '&date_end=' . date('Y:m:d H:i:s');
+$date_links['year'] = '?date_start=' . date('Y:m:d H:i:s', mktime(0, 0, 0, 1, 1))
+        . '&date_end=' . date('Y:m:d H:i:s');
+$date_links['all'] = '?date_start=' . date('Y:m:d H:i:s', 0)
+        . '&date_end=' . date('Y:m:d H:i:s', 2147483647);
+
+foreach ($date_links as $key => $value) {
+    if (isset($_GET['by_tag']))
+        $date_links[$key] = $value . '&by_tag=' . $_GET['by_tag'];
+    if (isset($_GET['page']))
+        $date_links[$key] = $value . '&page=' . $_GET['page'];
+}
+
+Page::addVar( 'date_links', $date_links );
+Page::addVar( 'date_start', isset($_GET['date_start']) ? $_GET['date_start'] : date('Y:m:d H:i:s', 0) );
+Page::addVar( 'date_end', isset($_GET['date_end']) ? $_GET['date_end'] : date('Y:m:d H:i:s', 2147483647) );
+
+/* Построение базовой ссылки для тегов
+ */
+
+$bl4t = array(); // base link for tags
+if (isset($_GET['date_start']))
+    $bl4t[] = 'date_start=' . $_GET['date_start'];
+
+if (isset($_GET['date_end']))
+    $bl4t[] = 'date_end=' . $_GET['date_end'];
+
+$bl4t = count($bl4t) > 0 ? '?' . implode('&', $bl4t) . '&' : '?';
+
+Page::addVar( 'bl4t', $bl4t );
+
+/* Параметры выборки
+ */
+
+$select = array();
+
+if (isset($_GET['by_tag'])) {
+    $select[] = array(
+        'text' => 'тег: <b>' .
+            Db::selectGetValue('SELECT name FROM tags WHERE id = @i', $_GET['by_tag']) . '</b>',
+        'link' => linkWithoutParam( 'by_tag' )
+    );
+}
+if (isset($_GET['date_start'])) {
+    $select[] = array(
+        'text' => 'дата с: <b>' . Util::readlyTime(strtotime($_GET['date_start'])) . '</b>',
+        'link' => linkWithoutParam( 'date_start' )
+    );
+}
+if (isset($_GET['date_end'])) {
+    $select[] = array(
+        'text' => 'дата по: <b>' . Util::readlyTime(strtotime($_GET['date_end'])) . '</b>',
+        'link' => linkWithoutParam( 'date_end' )
+    );
+}
+
+function linkWithoutParam ( $param ) {
+    $tmp = $_GET;
+    unset ( $tmp[ $param ] );
+    foreach ($tmp as $key => $value)
+        $tmp[$key] = "$key=$value";
+    return '?' . implode('&', $tmp);
+}
+
+Page::addVar( 'select', $select );
+
+/* Всё готово, осталось отрисовать страницу
+ */
+
 Page::draw( 'list' );
