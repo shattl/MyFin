2298ca148c53ea14e908708cfedf1d8b350d1fe5
Pominov Roman <meshokmuki@gmail.com>
2010-11-15 01:43:20 +0300

    1.0.13 ??????? ???

--- app/init.php
+++ app/init.php
@@ -1,6 +1,24 @@
 <?php
+// Убираем слеши понаставленые magic quotes
+if (get_magic_quotes_gpc()) {
+    function stripslashes_gpc(&$value)
+    {
+        $value = stripslashes($value);
+    }
+    array_walk_recursive($_GET, 'stripslashes_gpc');
+    array_walk_recursive($_POST, 'stripslashes_gpc');
+    array_walk_recursive($_COOKIE, 'stripslashes_gpc');
+    array_walk_recursive($_REQUEST, 'stripslashes_gpc');
+}
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+session_start();
+
+define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 
 require_once 'config.php';
+
+date_default_timezone_set(get_config('timezone'));
 
 // HTTP-авторизация
 if (get_config('auth_login') != '') {
@@ -17,12 +35,6 @@ require_once 'model/Page.php';
 require_once 'model/Util.php';
 require_once 'model/Messages.php';
 require_once 'model/Db.php';
-
-date_default_timezone_set(get_config('timezone'));
-setlocale(LC_ALL, 'ru_RU.UTF-8');
-session_start();
-
-define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 
 Page::set_scripts_dir(APPLICATION_PATH . '/view');
 Page::set_layout('layout');
