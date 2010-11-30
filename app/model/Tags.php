6c2670ca9bdfdb742fe698953f52c12c404dad74
Roman Pominov <meshokmuki@gmail.com>
2010-11-30 23:19:48 +0300

    ???????????? ????

--- app/model/Tags.php
+++ app/model/Tags.php
@@ -3,8 +3,8 @@
 class Tags {
 
     public static function getByEvent($ev_id) {
-        return Db::selectGetArray('SELECT t.* FROM `tags` AS t, `ev2tag` AS e2t WHERE'
-                . ' t.id = e2t.tag_id AND e2t.ev_id = @i ORDER BY t.name', $ev_id);
+        return self::patchColors(Db::selectGetArray('SELECT t.* FROM `tags` AS t, `ev2tag` AS e2t WHERE'
+                . ' t.id = e2t.tag_id AND e2t.ev_id = @i ORDER BY t.name', $ev_id));
     }
 
     public static function getOnlyNamesByEvent($ev_id) {
@@ -13,8 +13,8 @@ class Tags {
     }
 
     public static function getAllUsed() {
-        return Db::selectGetVerticalArray('SELECT t.name FROM tags AS t, ev2tag AS e2t'
-                . ' WHERE t.id = e2t.tag_id AND e2t.user_id = @i GROUP BY e2t.tag_id', User::getId());
+        return self::patchColors(Db::selectGetVerticalArray('SELECT t.name FROM tags AS t, ev2tag AS e2t'
+                . ' WHERE t.id = e2t.tag_id AND e2t.user_id = @i GROUP BY e2t.tag_id', User::getId()));
     }
 
     public static function nameById($tag_id) {
@@ -22,9 +22,9 @@ class Tags {
     }
 
     public static function tags4Cloud() {
-        return Db::selectGetArray('SELECT tags.*, count(ev2tag.ev_id) as count FROM ev2tag, tags '
+        return self::patchColors(Db::selectGetArray('SELECT tags.*, count(ev2tag.ev_id) as count FROM ev2tag, tags '
                 . 'WHERE ev2tag.tag_id = tags.id AND ev2tag.user_id = @i'
-                . ' GROUP BY ev2tag.tag_id ORDER BY count DESC, name', User::getId());
+                . ' GROUP BY ev2tag.tag_id ORDER BY count DESC, name', User::getId()));
     }
 
     public static function getIdByName($tag_name) {
@@ -62,6 +62,25 @@ class Tags {
         }
 
         return true;
+    }
+
+    public static function colorByName($name) {
+        $max = 160;
+        $min = 60;
+        $tmp = substr(md5($name), 0, 6);
+        $tmp = str_split($tmp, 2);
+        $tmp1 = '';
+        foreach ($tmp as $t)
+            $tmp1 .= sprintf('%02x', intval(hexdec($t) * ($max-$min) / 255) + $min);
+        return '#' . $tmp1;
+    }
+
+    private static function patchColors(&$tag_list){
+        foreach ($tag_list as $i => $t) {
+            $tag_list[$i]['color'] = self::colorByName($t['name']);
+            $tag_list[$i]['color_i'] = self::colorByName($t['name']);
+        }
+        return $tag_list;
     }
 
 }
