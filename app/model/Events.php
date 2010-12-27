675f32dabc82ba4656c7c318b3d4d5abe5d02869
Roman Pominov <meshokmuki@gmail.com>
2010-12-27 18:19:12 +0300

    ????????? ??? + ??????????? ??????? ? ???????? (???? ????-?? ?????)

--- app/model/Events.php
+++ app/model/Events.php
@@ -27,7 +27,7 @@ class Events {
         if (isset($get['by_tag'])) {
             $tags = explode(',', $get['by_tag']);
 
-            $sql = 'SELECT count(DISTINCT tag_id) AS tid, events.* FROM'
+            $sql = 'SELECT SQL_CALC_FOUND_ROWS count(DISTINCT tag_id) AS tid, events.* FROM'
                     . ' ev2tag LEFT JOIN events ON ev2tag.ev_id = events.id'
                     . (count($where) > 0 ? ' WHERE ' . implode(' AND ', $where) . ' AND ' : ' WHERE ')
                     . Db::buildReq('tag_id IN @a GROUP BY ev_id HAVING tid = @i', $tags, count($tags))
